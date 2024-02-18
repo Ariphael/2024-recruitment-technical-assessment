1b509beab32dc0240281780c71f951e5d9b77da2
Richard Tochtermann <richard.tochtermann@outlook.com>
2024-02-19 05:47:28 +1100

    q1 chaos technical task

--- chaos/src/data.rs
+++ chaos/src/data.rs
@@ -1,22 +1,41 @@
 use axum::{http::StatusCode, response::IntoResponse, Json};
 use serde::{Deserialize, Serialize};
 
-pub async fn process_data(Json(request): Json<DataRequest>) -> impl IntoResponse {
-    // Calculate sums and return response
-
-    let response = DataResponse {
-        
-    };
-
-    (StatusCode::OK, Json(response))
+#[derive(Debug, Deserialize)]
+#[serde(untagged)]
+pub enum Data {
+    Int(u32),
+    String(String),
 }
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 pub struct DataRequest {
-    // Add any fields here
+    data: Vec<Data>
 }
 
 #[derive(Serialize)]
 pub struct DataResponse {
-    // Add any fields here
+    string_len: u32,
+    int_sum: u32
+}
+
+pub async fn process_data(Json(request): Json<DataRequest>) -> impl IntoResponse {
+    println!("Received JSON data: {:?}", request);
+    // Calculate sums and return response
+    let mut string_len: u32 = 0;
+    let mut int_sum: u32 = 0;
+
+    for data in request.data.iter() {
+        match data {
+            Data::String(string_data) => string_len += string_data.len() as u32,
+            Data::Int(int_data) => int_sum += *int_data as u32,
+        }
+    }
+
+    let response = DataResponse {
+        string_len,
+        int_sum
+    };
+
+    (StatusCode::OK, Json(response))
 }
